d6de841d193f10879b0f18b48dd4e33d56886854
Artem Demchenkov <lunoxot@mail.ru>
2012-10-28 14:51:00 +0400

    version 1.2.1

---
+++ Artem@Demchenkov.ImageSpider/chrome/content/function.js
@@ -0,0 +1,296 @@
+window.addEventListener("load", function() {
+    imagepar.init();
+}, false);
+
+var imagepar=
+{
+    // for all loading pages
+	
+    init: function() {  
+	var appcontent = document.getElementById("appcontent");   // browser 
+	if(appcontent)  
+	    appcontent.addEventListener("DOMContentLoaded", imagepar.onPageLoad, true);
+    },  
+  
+    onPageLoad: function(aEvent) {  
+	var doc = aEvent.originalTarget; // doc is document that triggered "onload" event  
+	var is_opened = document.getElementById("imageparams_taskbar");
+	if(ImageSpider_PrefService.prefHasUserValue("spider_open"))
+	{	
+	    if(ImageSpider_PrefService.getIntPref("spider_open")==1)
+	    {
+		var is_opened = document.getElementById("imageparams_taskbar");
+		is_opened.setAttribute("checked", true);
+		imagepar.scan();
+	    }
+	}
+    },
+	
+    // about
+	
+    about: function()
+    {
+	window.openDialog("chrome://imageparams/content/about/about.xul", "imageparams_about", "centerscreen,chrome,modal");
+    },
+	
+    // search all frames in page
+	
+    documents: function(frame)
+    {
+	var all_documents = new Array();
+
+	if(frame)
+	{
+	    var frames=frame.frames;
+	    var framesLength=frames.length;
+
+	    if(frame.document)
+	    {
+		all_documents.push(frame.document);
+	    }
+
+	    for(var i=0; i<framesLength; i++)
+	    {
+		all_documents = all_documents.concat(imagepar.documents(frames[i]));
+	    }
+	}
+	
+	return all_documents;
+    },
+	
+    // remove our styles, scripts and developments
+	
+    remove: function()
+    {
+	var for_borders = (ImageSpider_PrefService.prefHasUserValue("for_borders")) ? ImageSpider_PrefService.getIntPref("for_borders") : 2;
+		
+	var content=imagepar.documents(window.top.getBrowser().selectedBrowser.contentWindow);
+		
+	for(var i=0; i<content.length; i++)
+	{
+	    for(var n=0; n<content[i].images.length; n++)
+	    {
+		content[i].images[n].removeAttribute("onmouseover");
+		content[i].images[n].removeAttribute("onmousemove");
+		content[i].images[n].removeAttribute("onmouseout");
+		if(for_borders==2) content[i].images[n].style.border='none';
+	    }
+			
+	    var For_script = content[i].getElementById("thisIsOurScript");
+	    content[i].body.removeChild(For_script);
+			
+	    var For_style = content[i].getElementById("thisIsOurStyle");
+	    content[i].body.removeChild(For_style);
+	}
+    },
+	
+    // run by all images and create our styles, scripts and developments
+	
+    scan: function()
+    {
+	var content=imagepar.documents(window.top.getBrowser().selectedBrowser.contentWindow);
+		
+	var font_color = (ImageSpider_PrefService.prefHasUserValue("text_color")) ? ImageSpider_PrefService.getComplexValue("text_color", Components.interfaces.nsISupportsString).data : "#000000";
+	var bg_color = (ImageSpider_PrefService.prefHasUserValue("bg_color")) ? ImageSpider_PrefService.getComplexValue("bg_color", Components.interfaces.nsISupportsString).data : "#f9fbdd";
+	var border_color = (ImageSpider_PrefService.prefHasUserValue("border_color")) ? ImageSpider_PrefService.getComplexValue("border_color", Components.interfaces.nsISupportsString).data : "#df0404";
+	var border_tool_color = (ImageSpider_PrefService.prefHasUserValue("border_tool_color")) ? ImageSpider_PrefService.getComplexValue("border_tool_color", Components.interfaces.nsISupportsString).data : "#000000";
+	var font_size = (ImageSpider_PrefService.prefHasUserValue("font_size")) ? ImageSpider_PrefService.getIntPref("font_size") : 11;
+	var opacity = (ImageSpider_PrefService.prefHasUserValue("opacity")) ? ImageSpider_PrefService.getIntPref("opacity") : 8;
+	var for_borders = (ImageSpider_PrefService.prefHasUserValue("for_borders")) ? ImageSpider_PrefService.getIntPref("for_borders") : 2;
+		
+	for(var i=0; i<content.length; i++)
+	{
+	    var For_style = content[i].createElement('style');
+	    content[i].body.appendChild(For_style);
+	    For_style.setAttribute("id", "thisIsOurStyle");
+	    For_style.innerHTML=" \
+				div.image_information{ \
+				padding:2px; \
+				-moz-opacity: 0."+opacity+"; \
+				opacity: 0."+opacity+"; \
+				background-color:"+bg_color+"; \
+				font-size:"+font_size+"px; \
+				font-family:Arial,Verdana; \
+				font-weight:normal; \
+				font-style:normal; \
+				color:"+font_color+"; \
+				border:1px solid "+border_tool_color+"; \
+				text-align:left;\
+				position:absolute; \
+				z-index:999;} \
+			";
+			
+	    var For_script = content[i].createElement('script');
+	    content[i].body.appendChild(For_script);
+	    For_script.setAttribute("id", "thisIsOurScript");
+	    var showFunction="\
+				divVis=0; \
+				ourAlt=''; \
+				ourTitle=''; \
+				\
+				function showThis(where, e)\
+				{ \
+					var For_content =document.createElement('div'); \
+					document.body.appendChild(For_content); \
+					For_content.className='image_information'; \
+					For_content.setAttribute('id','image_informer'); \
+					\
+					var topLeft=cursorPosition(e); \
+					var newX=topLeft[0]+13; \
+					var newY=topLeft[1]+10; \
+					For_content.style.left=newX+'px'; \
+					For_content.style.top=newY+'px'; \
+					\
+					ourAlt=where.alt; \
+					ourTitle=where.title; \
+					var outHtml=''; \
+					\
+					if(where.src) outHtml=outHtml+'src: '+where.src+'<br/>'; \
+					if(where.naturalWidth) outHtml=outHtml+'natural width: '+where.naturalWidth+'px;<br/>'; \
+					if(where.naturalHeight) outHtml=outHtml+'natural height: '+where.naturalHeight+'px;<br/>'; \
+					if(where.width) outHtml=outHtml+'width: '+where.width+'px;<br/>'; \
+					if(where.height) outHtml=outHtml+'height: '+where.height+'px;<br/>'; \
+					if(ourAlt) outHtml=outHtml+'alt: '+ourAlt+'<br/>'; \
+					if(ourTitle) outHtml=outHtml+'title: '+ourTitle+'<br/>'; \
+					if(where.id) outHtml=outHtml+'id: '+where.id+'<br/>'; \
+					if(where.className) outHtml=outHtml+'class: '+where.className+'<br/>'; \
+					if(where.border) outHtml=outHtml+'border: '+where.border+'<br/>'; \
+					\
+					For_content.innerHTML=outHtml; \
+					divVis=1; \
+					where.removeAttribute('alt'); \
+					where.removeAttribute('title'); \
+				} \
+				\
+				function hideThis(where) \
+				{ \
+					document.body.removeChild(document.getElementById('image_informer')); \
+					where.setAttribute('alt',ourAlt); \
+					where.setAttribute('title',ourTitle); \
+					divVis=0; \
+					ourAlt=''; \
+					ourTitle=''; \
+				} \
+				\
+				function moveThis(e) \
+				{ \
+					if(divVis==1) \
+					{ \
+						var topLeft=cursorPosition(e); \
+						var newX=topLeft[0]+13; \
+						var newY=topLeft[1]+10; \
+						document.getElementById('image_informer').style.left=newX+'px'; \
+						document.getElementById('image_informer').style.top=newY+'px'; \
+					} \
+				} \
+				\
+				function cursorPosition(e) \
+				{ \
+					var xPlusY= new Array(2); \
+					\
+					if (e.pageX || e.pageY) \
+					{ \
+						var x=e.pageX; \
+						var y=e.pageY; \
+					} \
+					else if (e.clientX || e.clientY) \
+					{ \
+						var x=e.clientX+(document.documentElement.scrollLeft || document.body.scrollLeft)-document.documentElement.clientLeft; \
+						var y=e.clientY+(document.documentElement.scrollTop || document.body.scrollTop)-document.documentElement.clientTop; \
+					} \
+					xPlusY[0]=x; \
+					xPlusY[1]=y; \
+					\
+					return xPlusY; \
+				} \
+			";
+			
+	    For_script.innerHTML=showFunction;
+			
+	    for(var n=0; n<content[i].images.length; n++)
+	    {
+		if(for_borders==2)
+		{
+		    content[i].images[n].style.border='1px solid '+border_color;
+		    content[i].images[n].setAttribute("onmouseover", "showThis(this, event);");
+		    content[i].images[n].setAttribute("onmouseout", "hideThis(this);");
+		}
+		else
+		{
+		    content[i].images[n].setAttribute("onmouseover", "showThis(this, event); this.style.border='1px solid "+border_color+"';");
+		    content[i].images[n].setAttribute("onmouseout", "hideThis(this); this.style.border='none';");
+		}
+				
+		content[i].images[n].setAttribute("onmousemove", "moveThis(event);");
+	    }
+	}
+    },
+	
+    settings: function()
+    {	
+	window.openDialog("chrome://imageparams/content/settings/settings.xul", "imageparams_settings", "centerscreen,chrome,modal,resizable");
+    },
+	
+    // run Image Spider from menu
+	
+    toggleImagepar: function (is_opened)
+    {
+	if(is_opened.getAttribute("checked"))
+	{
+	    imagepar.scan();
+	    ImageSpider_PrefService.setIntPref("spider_open", 1);
+	}
+	else
+	{
+	    imagepar.remove();
+	    ImageSpider_PrefService.setIntPref("spider_open", 0);
+	}
+    },
+	
+    // run Image Spider from toolbar
+	
+    toggleImageparMenu: function ()
+    { 		
+	var is_opened = document.getElementById("imageparams_taskbar");
+		
+	if(is_opened.getAttribute("checked")=="true")
+	{
+	    imagepar.remove();
+	    is_opened.setAttribute("checked", false);
+	    ImageSpider_PrefService.setIntPref("spider_open", 0);
+	}
+	else
+	{
+	    imagepar.scan();
+	    is_opened.setAttribute("checked", true);
+	    ImageSpider_PrefService.setIntPref("spider_open", 1);
+	}
+    },
+	
+    // check flag in checkbox
+	
+    updateImageparPopUp: function()
+    {
+	var content=imagepar.documents(window.top.getBrowser().selectedBrowser.contentWindow);
+	var script_number=0;
+		
+	for(var i=0; i<content.length; i++)
+	{
+	    if(content[i].getElementById("thisIsOurScript"))
+	    {
+		script_number=script_number+1;
+	    }
+	}
+		
+	if(script_number==0)
+	{
+	    is_opened = document.getElementById("imageparams_taskbar");
+	    document.getElementById("imageparams_taskbar").setAttribute("checked", false);
+	}
+	else
+	{
+	    is_opened = document.getElementById("imageparams_taskbar");
+	    document.getElementById("imageparams_taskbar").setAttribute("checked", true);
+	}
+    }
+}
---
+++ Artem@Demchenkov.ImageSpider/chrome/content/settings/settings.js
@@ -0,0 +1,125 @@
+const ImageSpider_PrefService=Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefService).getBranch("ImageSpider_tutorial.");
+
+var spiderSettings=
+{		
+    read: function()
+    {	
+	if(ImageSpider_PrefService.prefHasUserValue("text_color"))
+	{
+	    var font_color= ImageSpider_PrefService.getComplexValue("text_color", Components.interfaces.nsISupportsString).data;
+	    document.getElementById("imageparams_text_color").color=font_color;
+	}
+	else
+	{
+	    document.getElementById("imageparams_text_color").color="#000000";
+	}
+		
+	if(ImageSpider_PrefService.prefHasUserValue("bg_color"))
+	{
+	    var bg_color= ImageSpider_PrefService.getComplexValue("bg_color", Components.interfaces.nsISupportsString).data;
+	    document.getElementById("imageparams_bg_color").color=bg_color;
+	}
+	else
+	{
+	    document.getElementById("imageparams_bg_color").color="#f9fbdd";
+	}
+		
+	if(ImageSpider_PrefService.prefHasUserValue("border_color"))
+	{
+	    var border_color= ImageSpider_PrefService.getComplexValue("border_color", Components.interfaces.nsISupportsString).data;
+	    document.getElementById("imageparams_border_color").color=border_color;
+	}
+	else
+	{
+	    document.getElementById("imageparams_border_color").color="#df0404";
+	}
+		
+	if(ImageSpider_PrefService.prefHasUserValue("border_tool_color"))
+	{
+	    var border_tool_color= ImageSpider_PrefService.getComplexValue("border_tool_color", Components.interfaces.nsISupportsString).data;
+	    document.getElementById("imageparams_border_tool_color").color=border_tool_color;
+	}
+	else
+	{
+	    document.getElementById("imageparams_border_tool_color").color="#000000";
+	}
+		
+	if(ImageSpider_PrefService.prefHasUserValue("font_size"))
+	{
+	    var font_size= ImageSpider_PrefService.getIntPref("font_size");
+	    document.getElementById("imageparams_text_size").value=font_size;
+	}
+	else
+	{
+	    document.getElementById("imageparams_text_size").value=11;
+	}
+		
+	if(ImageSpider_PrefService.prefHasUserValue("opacity"))
+	{
+	    var opacity= ImageSpider_PrefService.getIntPref("opacity");
+	    document.getElementById("imageparams_text_opacity").value=opacity;
+	}
+	else
+	{
+	    document.getElementById("imageparams_text_opacity").value=8;
+	}
+		
+	if(ImageSpider_PrefService.prefHasUserValue("for_borders"))
+	{
+	    var for_borders= ImageSpider_PrefService.getIntPref("for_borders");
+	    document.getElementById("imageparams_for_borders").value=for_borders;
+	}
+	else
+	{
+	    document.getElementById("imageparams_for_borders").value=2;
+	}
+    },
+	
+    resetOptions: function()
+    {
+	document.getElementById("imageparams_text_color").color="#000000";
+	document.getElementById("imageparams_bg_color").color="#f9fbdd";
+	document.getElementById("imageparams_border_color").color="#df0404";
+	document.getElementById("imageparams_border_tool_color").color="#000000";
+	document.getElementById("imageparams_text_size").value=11;
+	document.getElementById("imageparams_text_opacity").value=8;
+	document.getElementById("imageparams_for_borders").value=2;
+    },
+	
+    save: function()
+    {
+	var supportsStringInterface=Components.interfaces.nsISupportsString;
+		
+	var ImageSpider_font_color=document.getElementById("imageparams_text_color");
+	var string_font=Components.classes["@mozilla.org/supports-string;1"].createInstance(supportsStringInterface);
+		
+	var ImageSpider_bg_color=document.getElementById("imageparams_bg_color");
+	var string_bg=Components.classes["@mozilla.org/supports-string;1"].createInstance(supportsStringInterface);
+		
+	var ImageSpider_border_color=document.getElementById("imageparams_border_color");
+	var string_border=Components.classes["@mozilla.org/supports-string;1"].createInstance(supportsStringInterface);
+		
+	var ImageSpider_border_tool_color=document.getElementById("imageparams_border_tool_color");
+	var string_border_tool=Components.classes["@mozilla.org/supports-string;1"].createInstance(supportsStringInterface);
+		
+	var ImageSpider_font_size=document.getElementById("imageparams_text_size").value;
+		
+	var ImageSpider_opacity=document.getElementById("imageparams_text_opacity").value;
+		
+	var ImageSpider_for_borders=document.getElementById("imageparams_for_borders").value;
+
+	string_font.data = ImageSpider_font_color.color;
+	string_bg.data = ImageSpider_bg_color.color;
+	string_border.data = ImageSpider_border_color.color;
+	string_border_tool.data = ImageSpider_border_tool_color.color;
+
+	ImageSpider_PrefService.setComplexValue("text_color", supportsStringInterface, string_font);
+	ImageSpider_PrefService.setComplexValue("bg_color", supportsStringInterface, string_bg);
+	ImageSpider_PrefService.setComplexValue("border_color", supportsStringInterface, string_border);
+	ImageSpider_PrefService.setComplexValue("border_tool_color", supportsStringInterface, string_border_tool);
+		
+	ImageSpider_PrefService.setIntPref("font_size", ImageSpider_font_size);
+	ImageSpider_PrefService.setIntPref("opacity", ImageSpider_opacity);
+	ImageSpider_PrefService.setIntPref("for_borders", ImageSpider_for_borders);
+    }
+}
